103c36fdf267d0b66d14da26d20bfa63de351f22
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-12-22 20:33:59 +0100

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './Filter/Filter'
 import Contacts from './Contacts/Contacts'
 import FormUser  from './FormUser/FormUser'
